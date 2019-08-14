d003bd7ba786e465bdcacc8ded72e66c0a35ca6c
jasonczc <jasonczc@qq.com>
2019-08-14 17:22:52 +0800

    update config

---
+++ proxy/web/commandThing.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jasonczc
+ * Date: 2019-08-14
+ * Time: 17:05
+ */
+
---
+++ proxy/web/generateConfig.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jasonczc
+ * Date: 2019-08-14
+ * Time: 17:15
+ */
